a4860823fc28abca7f7f859834b2639801e3f118
Ankan <ankan.makaut@gmail.com>
2025-07-23 18:02:51 +0530

    version 1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# linear_regression_model
+# Linear Regression from Scratch
+
+A Python implementation of linear regression models matching scikit-learn's API, including:
+- Ordinary Least Squares (closed-form solution)
+- Gradient Descent (iterative approach)
+
+## Installation
+```bash
+pip install git+https://github.com/ankan62/linear_regression_model.git
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="linear_regression_model",
+    version="0.1.0",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="Linear regression implementation from scratch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ankan62/linear_regression_model",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "numpy>=1.20.0",
+    ],
+)
